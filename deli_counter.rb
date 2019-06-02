8ea5504cb969a81e79bbc27b9a102cbab539df45
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-02 23:55:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,13 +2,19 @@
 # Write your code here.
 katz_deli = []
 
-def line(array)
-  current_line = "The line is currently:"
-    array.each.with_index(1) do |value, indexemus|
-      current_line << " #{indexemus}. #{value},"
-    end
-    puts current_line
+def line(array) 
+  if array.length >= 1
+    nuarray = []
+    counter = 1 
+    array.each do |name|
+      nuarray.push("#{counter}. #{name}")
+      counter += 1 
+    end 
+    puts "The line is currently: #{nuarray.join(" ")}"
+  else
+    puts "The line is currently empty."
   end
+end
 
 def take_a_number(line,new_person)
   line.push(new_person)
